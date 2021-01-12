ab71cbc2e4dedcd8cf16c3c2589f673cb6634567
MBrassey <luc1d_life@protonmail.com>
2021-01-11 19:08:27 -0700

    Implemented Reaction Mutation

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,39 +1,17 @@
 import React, { useState } from "react";
 
 import { useMutation } from "@apollo/react-hooks";
-import { ADD_THOUGHT } from "../../utils/mutations";
-import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
+import { ADD_REACTION } from "../../utils/mutations";
 
-const ThoughtForm = () => {
-  const [thoughtText, setText] = useState("");
+const ReactionForm = ({ thoughtId }) => {
+  const [reactionBody, setBody] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
-  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
-    update(cache, { data: { addThought } }) {
-      try {
-        // update thought array's cache
-        // could potentially not exist yet, so wrap in a try/catch
-        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
-        cache.writeQuery({
-          query: QUERY_THOUGHTS,
-          data: { thoughts: [addThought, ...thoughts] },
-        });
-      } catch (e) {
-        console.error(e);
-      }
-
-      // update me object's cache
-      const { me } = cache.readQuery({ query: QUERY_ME });
-      cache.writeQuery({
-        query: QUERY_ME,
-        data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
-      });
-    },
-  });
+  const [addReaction, { error }] = useMutation(ADD_REACTION);
 
   // update state based on form input changes
   const handleChange = (event) => {
     if (event.target.value.length <= 280) {
-      setText(event.target.value);
+      setBody(event.target.value);
       setCharacterCount(event.target.value.length);
     }
   };
@@ -43,13 +21,12 @@ const ThoughtForm = () => {
     event.preventDefault();
 
     try {
-      // add thought to database
-      await addThought({
-        variables: { thoughtText },
+      await addReaction({
+        variables: { reactionBody, thoughtId },
       });
 
       // clear form value
-      setText("");
+      setBody("");
       setCharacterCount(0);
     } catch (e) {
       console.error(e);
@@ -69,17 +46,20 @@ const ThoughtForm = () => {
         onSubmit={handleFormSubmit}
       >
         <textarea
-          placeholder="Here's a new thought..."
-          value={thoughtText}
+          placeholder="Leave a reaction to this thought..."
+          value={reactionBody}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         ></textarea>
+
         <button className="btn col-12 col-md-3" type="submit">
           Submit
         </button>
       </form>
+
+      {error && <div>Something went wrong...</div>}
     </div>
   );
 };
 
-export default ThoughtForm;
+export default ReactionForm;
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Auth from "../utils/auth";
+import ReactionList from "../components/ReactionList";
+import ReactionForm from "../components/ReactionForm";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_THOUGHT } from "../utils/queries";
 
-import ReactionList from '../components/ReactionList';
-
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHT } from '../utils/queries';
-
-const SingleThought = props => {
+const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
+    variables: { id: thoughtId },
   });
 
   const thought = data?.thought || {};
@@ -25,7 +25,7 @@ const SingleThought = props => {
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
-          </span>{' '}
+          </span>{" "}
           thought on {thought.createdAt}
         </p>
         <div className="card-body">
@@ -33,7 +33,10 @@ const SingleThought = props => {
         </div>
       </div>
 
-      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
