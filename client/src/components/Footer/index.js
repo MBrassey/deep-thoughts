5e4d067369f574b6e22c91aca3a95e295a633506
MBrassey <luc1d_life@protonmail.com>
2021-01-11 20:11:58 -0700

    Prepared for deployment

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Lernantino</div>
+      <div className="container">Matt Brassey 2021</div>
     </footer>
   );
 };
